1c5030db449fccbb8d92e915a2a9c4186ba4a641
AmauriBlanco <blancoamauri@gmail.com>
2024-05-21 21:32:54 -0300

    composables

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,12 @@
+<script setup>
+import { useFlash } from '@/composables/useFlash'
+
+let { flash } = useFlash()
+</script>
 <template>
   <div>
     <p>
-      <button @click="flash('Id Works on The About page')">Click Me</button>
+      <button @click="flash('Yay!', 'Id Works on The About page', 'info')">Click Me</button>
     </p>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,13 @@
-<script>
-export default {
-  methods: {
-    flash(message) {
-      alert(message)
-    }
-  }
-}
+<script setup>
+import { useFlash } from '@/composables/useFlash'
+
+let { flash } = useFlash()
 </script>
 
 <template>
   <main>
     <p>
-      <button @click="flash('Id Works')">Click Me</button>
+      <button @click="flash('Teste', 'Id Works')">Click Me</button>
     </p>
   </main>
 </template>
